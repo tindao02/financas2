b29ea219dee2876714721158ac8e502048690ccc
Manoel Tindão <timdao02@hotmail.com>
2020-05-16 18:23:28 -0300

    Criação do projeto

---
+++ src/main/java/com/tindao/financas/Financas2Application.java
@@ -0,0 +1,13 @@
+package com.tindao.financas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Financas2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Financas2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/tindao/financas/Financas2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tindao.financas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Financas2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
